d795ff738be4bed64e3d57d28e7a31a7be9e6926
jripke74 <jripke74@gmail.com>
2020-06-16 14:23:43 -0700

    update to use modules

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,5 @@
+import { ProjectList } from './app/ProjectList.js';
+
 class App {
   static init() {
     const activeProjectsList = new ProjectList('active');
--- assets/scripts/app/ProjectList.js
+++ assets/scripts/app/ProjectList.js
@@ -1,4 +1,8 @@
-class ProjectList {
+import { ProjectItem } from './ProjectItem.js'
+import { DOMHelper } from '../Utility/DOMHelper.js'
+
+export class ProjectList {
+
   projects = [];
 
   constructor(type) {
